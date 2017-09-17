c9f4ee7cde854d4e8c037f22b01bda37c435eb02
evgeniy-mh <djpon94@gmail.com>
2017-09-17 16:52:10 +0400

    added lectures list template

--- TutorialBook/Form1.Designer.cs
+++ TutorialBook/Form1.Designer.cs
@@ -28,21 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.CppBookTab = new System.Windows.Forms.TabPage();
+            this.LecturesTreeView = new System.Windows.Forms.TreeView();
+            this.ExercisesTab = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.CppBookTab.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.CppBookTab);
+            this.tabControl1.Controls.Add(this.ExercisesTab);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(656, 486);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // CppBookTab
+            // 
+            this.CppBookTab.Controls.Add(this.LecturesTreeView);
+            this.CppBookTab.Location = new System.Drawing.Point(4, 22);
+            this.CppBookTab.Name = "CppBookTab";
+            this.CppBookTab.Padding = new System.Windows.Forms.Padding(3);
+            this.CppBookTab.Size = new System.Drawing.Size(648, 460);
+            this.CppBookTab.TabIndex = 0;
+            this.CppBookTab.Text = "Учебные материалы";
+            this.CppBookTab.UseVisualStyleBackColor = true;
+            // 
+            // LecturesTreeView
+            // 
+            this.LecturesTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LecturesTreeView.Location = new System.Drawing.Point(7, 7);
+            this.LecturesTreeView.Name = "LecturesTreeView";
+            this.LecturesTreeView.Size = new System.Drawing.Size(305, 447);
+            this.LecturesTreeView.TabIndex = 0;
+            // 
+            // ExercisesTab
+            // 
+            this.ExercisesTab.Location = new System.Drawing.Point(4, 22);
+            this.ExercisesTab.Name = "ExercisesTab";
+            this.ExercisesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ExercisesTab.Size = new System.Drawing.Size(648, 460);
+            this.ExercisesTab.TabIndex = 1;
+            this.ExercisesTab.Text = "Задания";
+            this.ExercisesTab.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(680, 510);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Учебник C++";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.CppBookTab.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage CppBookTab;
+        private System.Windows.Forms.TabPage ExercisesTab;
+        private System.Windows.Forms.TreeView LecturesTreeView;
     }
 }
 
--- TutorialBook/Form1.cs
+++ TutorialBook/Form1.cs
@@ -11,14 +11,34 @@ using System.Windows.Forms;
 namespace TutorialBook
 {
     public partial class Form1 : Form
-    {
+    {        
         public Form1()
         {
             InitializeComponent();
+
+            initLecuresTreeView();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void initLecuresTreeView()
+        {
+            TreeNode[] lectures = {
+                new TreeNode("Лекция 1. Объектно-ориентированное программирование",new TreeNode[] {
+                    new TreeNode("1.1.	Введение"),
+                    new TreeNode("1.2.	Понятие класса"),
+                    new TreeNode("1.3.	Принципы и свойства объектно-ориентированного программирования")
+                }),
+
+                new TreeNode("Лекция 2. Общие сведения о классах")
+            };
+
+            //lectures[0].Nodes.AddRange( {new TreeNode("sa") });
+
+            LecturesTreeView.Nodes.AddRange(lectures);
 
         }
     }
