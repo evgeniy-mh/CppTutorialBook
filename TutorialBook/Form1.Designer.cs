c7c9d746d781133b911b8f1feaf63acfab3c3e54
evgeniy-mh <djpon94@gmail.com>
2017-09-18 18:48:36 +0400

    code refactor

--- TutorialBook/Form1.Designer.cs
+++ TutorialBook/Form1.Designer.cs
@@ -30,13 +30,20 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.CppBookTab = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LectureRichTextBox = new System.Windows.Forms.RichTextBox();
             this.LecturesTreeView = new System.Windows.Forms.TreeView();
             this.ExercisesTab = new System.Windows.Forms.TabPage();
-            this.LectureRichTextBox = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.UserCodeTextBox = new System.Windows.Forms.TextBox();
+            this.RunCodeButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ExercisesTreeView = new System.Windows.Forms.TreeView();
+            this.ExerciseTextTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.CppBookTab.SuspendLayout();
+            this.ExercisesTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -49,7 +56,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(656, 486);
+            this.tabControl1.Size = new System.Drawing.Size(986, 516);
             this.tabControl1.TabIndex = 0;
             // 
             // CppBookTab
@@ -61,51 +68,10 @@
             this.CppBookTab.Location = new System.Drawing.Point(4, 22);
             this.CppBookTab.Name = "CppBookTab";
             this.CppBookTab.Padding = new System.Windows.Forms.Padding(3);
-            this.CppBookTab.Size = new System.Drawing.Size(648, 460);
+            this.CppBookTab.Size = new System.Drawing.Size(978, 490);
             this.CppBookTab.TabIndex = 0;
             this.CppBookTab.Text = "Учебные материалы";
             this.CppBookTab.UseVisualStyleBackColor = true;
-            // 
-            // LecturesTreeView
-            // 
-            this.LecturesTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.LecturesTreeView.Location = new System.Drawing.Point(7, 23);
-            this.LecturesTreeView.Name = "LecturesTreeView";
-            this.LecturesTreeView.Size = new System.Drawing.Size(305, 431);
-            this.LecturesTreeView.TabIndex = 0;
-            this.LecturesTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.LecturesTreeView_AfterSelect);
-            // 
-            // ExercisesTab
-            // 
-            this.ExercisesTab.Location = new System.Drawing.Point(4, 22);
-            this.ExercisesTab.Name = "ExercisesTab";
-            this.ExercisesTab.Padding = new System.Windows.Forms.Padding(3);
-            this.ExercisesTab.Size = new System.Drawing.Size(648, 460);
-            this.ExercisesTab.TabIndex = 1;
-            this.ExercisesTab.Text = "Задания";
-            this.ExercisesTab.UseVisualStyleBackColor = true;
-            // 
-            // LectureRichTextBox
-            // 
-            this.LectureRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.LectureRichTextBox.Location = new System.Drawing.Point(318, 23);
-            this.LectureRichTextBox.Name = "LectureRichTextBox";
-            this.LectureRichTextBox.ReadOnly = true;
-            this.LectureRichTextBox.Size = new System.Drawing.Size(324, 431);
-            this.LectureRichTextBox.TabIndex = 1;
-            this.LectureRichTextBox.Text = "";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Список лекций";
             // 
             // label2
             // 
@@ -116,11 +82,111 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Содержание";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Список лекций";
+            // 
+            // LectureRichTextBox
+            // 
+            this.LectureRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LectureRichTextBox.Location = new System.Drawing.Point(318, 23);
+            this.LectureRichTextBox.Name = "LectureRichTextBox";
+            this.LectureRichTextBox.ReadOnly = true;
+            this.LectureRichTextBox.Size = new System.Drawing.Size(654, 461);
+            this.LectureRichTextBox.TabIndex = 1;
+            this.LectureRichTextBox.Text = "";
+            // 
+            // LecturesTreeView
+            // 
+            this.LecturesTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LecturesTreeView.Location = new System.Drawing.Point(7, 23);
+            this.LecturesTreeView.Name = "LecturesTreeView";
+            this.LecturesTreeView.Size = new System.Drawing.Size(305, 461);
+            this.LecturesTreeView.TabIndex = 0;
+            this.LecturesTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.LecturesTreeView_AfterSelect);
+            // 
+            // ExercisesTab
+            // 
+            this.ExercisesTab.Controls.Add(this.label4);
+            this.ExercisesTab.Controls.Add(this.ExerciseTextTextBox);
+            this.ExercisesTab.Controls.Add(this.ExercisesTreeView);
+            this.ExercisesTab.Controls.Add(this.label3);
+            this.ExercisesTab.Controls.Add(this.RunCodeButton);
+            this.ExercisesTab.Controls.Add(this.UserCodeTextBox);
+            this.ExercisesTab.Location = new System.Drawing.Point(4, 22);
+            this.ExercisesTab.Name = "ExercisesTab";
+            this.ExercisesTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ExercisesTab.Size = new System.Drawing.Size(978, 490);
+            this.ExercisesTab.TabIndex = 1;
+            this.ExercisesTab.Text = "Задачи";
+            this.ExercisesTab.UseVisualStyleBackColor = true;
+            // 
+            // UserCodeTextBox
+            // 
+            this.UserCodeTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserCodeTextBox.Location = new System.Drawing.Point(252, 257);
+            this.UserCodeTextBox.Multiline = true;
+            this.UserCodeTextBox.Name = "UserCodeTextBox";
+            this.UserCodeTextBox.Size = new System.Drawing.Size(285, 227);
+            this.UserCodeTextBox.TabIndex = 0;
+            // 
+            // RunCodeButton
+            // 
+            this.RunCodeButton.Location = new System.Drawing.Point(593, 418);
+            this.RunCodeButton.Name = "RunCodeButton";
+            this.RunCodeButton.Size = new System.Drawing.Size(75, 23);
+            this.RunCodeButton.TabIndex = 2;
+            this.RunCodeButton.Text = "Запуск";
+            this.RunCodeButton.UseVisualStyleBackColor = true;
+            this.RunCodeButton.Click += new System.EventHandler(this.RunCodeButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Задачи";
+            // 
+            // ExercisesTreeView
+            // 
+            this.ExercisesTreeView.Location = new System.Drawing.Point(7, 24);
+            this.ExercisesTreeView.Name = "ExercisesTreeView";
+            this.ExercisesTreeView.Size = new System.Drawing.Size(235, 460);
+            this.ExercisesTreeView.TabIndex = 4;
+            // 
+            // ExerciseTextTextBox
+            // 
+            this.ExerciseTextTextBox.Location = new System.Drawing.Point(249, 24);
+            this.ExerciseTextTextBox.Multiline = true;
+            this.ExerciseTextTextBox.Name = "ExerciseTextTextBox";
+            this.ExerciseTextTextBox.Size = new System.Drawing.Size(723, 122);
+            this.ExerciseTextTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Текст задачи";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 510);
+            this.ClientSize = new System.Drawing.Size(1010, 540);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Учебник C++";
@@ -128,6 +194,8 @@
             this.tabControl1.ResumeLayout(false);
             this.CppBookTab.ResumeLayout(false);
             this.CppBookTab.PerformLayout();
+            this.ExercisesTab.ResumeLayout(false);
+            this.ExercisesTab.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -141,6 +209,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox LectureRichTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button RunCodeButton;
+        private System.Windows.Forms.TextBox UserCodeTextBox;
+        private System.Windows.Forms.TreeView ExercisesTreeView;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ExerciseTextTextBox;
     }
 }
 
--- TutorialBook/Form1.cs
+++ TutorialBook/Form1.cs
@@ -12,35 +12,47 @@ using System.Windows.Forms;
 namespace TutorialBook
 {
     public partial class Form1 : Form
-    {        
+    {
+
+        public DirectoryInfo AppDirectory;
+        DirectoryInfo LecturesDirectory;
+        DirectoryInfo ExercisesDirectory;
 
         //хранится соответствие между названием файла в левом меню и самим файлом на диске
         private Dictionary<string,FileInfo> lectureFilesDictionary=new Dictionary<string, FileInfo>();
 
+        Microsoft.Office.Interop.Word.Application WordApplication;
+        Microsoft.Office.Interop.Word.Document WordDocument;
+
         public Form1()
         {
             InitializeComponent();
-
+            findAppDirectory();
             initLecuresTreeView();
+            initExercisesListView();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            WordApplication = new Microsoft.Office.Interop.Word.Application();
+            WordDocument = new Microsoft.Office.Interop.Word.Document();
         }
 
-        private void initLecuresTreeView()
+        private void findAppDirectory()
         {
             //идет вверх по папкам пока не попадет в главную папку программы где есть папка Lectures
-            DirectoryInfo AppDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            AppDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
 
-            while (!AppDirectory.EnumerateDirectories().Any((dir)=> { return dir.Name == "Lectures"; }))
+            while (!AppDirectory.EnumerateDirectories().Any((dir) => { return dir.Name == "Lectures"; }))
             {
                 AppDirectory = AppDirectory.Parent;
             }
+        }
 
+        private void initLecuresTreeView()
+        {   
             //переход в папку Lectures
-            DirectoryInfo LecturesDirectory = new DirectoryInfo(AppDirectory.FullName + @"\Lectures\");
+            LecturesDirectory = new DirectoryInfo(AppDirectory.FullName + @"\Lectures\");
             
             //добавление папок с содержанием каждой лекции в список лекций
             foreach (DirectoryInfo dir in LecturesDirectory.GetDirectories())
@@ -55,7 +67,15 @@ namespace TutorialBook
                 }
                 LecturesTreeView.Nodes.Add(new TreeNode(dir.Name,childNodes.ToArray()));
             }
+        }
 
+        private void initExercisesListView()
+        {
+            ExercisesDirectory = new DirectoryInfo(AppDirectory.FullName + @"\Exercises\");
+            foreach(DirectoryInfo dir in ExercisesDirectory.GetDirectories())
+            {
+                ExercisesTreeView.Nodes.Add(new TreeNode(dir.Name));
+            }
         }
 
         private void LecturesTreeView_AfterSelect(object sender, TreeViewEventArgs e)
@@ -67,16 +87,12 @@ namespace TutorialBook
             }          
         }
 
-
+        //TODO: REFACTOR!
         private void showFile(FileInfo file)
         {
-            LectureRichTextBox.ReadOnly = false;
-            LectureRichTextBox.Clear();
-            LectureRichTextBox.Text = "...loading file";
-
             object filename = file.FullName;
-            Microsoft.Office.Interop.Word.Application AC = new Microsoft.Office.Interop.Word.Application();
-            Microsoft.Office.Interop.Word.Document doc = new Microsoft.Office.Interop.Word.Document();
+            /*WordApplication = new Microsoft.Office.Interop.Word.Application();
+            document = new Microsoft.Office.Interop.Word.Document();*/
 
             object readOnly = false;
             object isVisible = true;
@@ -84,9 +100,13 @@ namespace TutorialBook
 
             try
             {
-                doc = AC.Documents.Open(ref filename, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible);
-                doc.Content.Select();
-                doc.Content.Copy();
+                LectureRichTextBox.ReadOnly = false;
+                LectureRichTextBox.Clear();
+                LectureRichTextBox.Text = "...loading file";
+
+                WordDocument = WordApplication.Documents.Open(ref filename, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible);
+                WordDocument.Content.Select();
+                WordDocument.Content.Copy();
 
                 LectureRichTextBox.Clear();
                 LectureRichTextBox.Paste();                
@@ -97,9 +117,14 @@ namespace TutorialBook
             }
             finally
             {
-                doc.Close(ref missing, ref missing, ref missing);
+                WordDocument.Close(ref missing, ref missing, ref missing);
                 LectureRichTextBox.ReadOnly = true;
             }
+        }
+
+        private void RunCodeButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("RunCodeButton_Click");
         }
     }
 }
