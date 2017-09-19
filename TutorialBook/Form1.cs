43140f192fd02be5a20157642dad10f21da2a511
evgeniy-mh <djpon94@gmail.com>
2017-09-19 19:30:10 +0400

    added template for compiling cpp files

--- TutorialBook/Form1.cs
+++ TutorialBook/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -17,9 +18,10 @@ namespace TutorialBook
         public DirectoryInfo AppDirectory;
         DirectoryInfo LecturesDirectory;
         DirectoryInfo ExercisesDirectory;
+        DirectoryInfo currentExerciseDirectory;
 
         //хранится соответствие между названием файла лекции в левом меню и самим файлом на диске
-        private Dictionary<string,FileInfo> lectureFilesDictionary=new Dictionary<string, FileInfo>();
+        private Dictionary<string, FileInfo> lectureFilesDictionary = new Dictionary<string, FileInfo>();
         private Dictionary<string, DirectoryInfo> exerciseDirectoriesDictionary = new Dictionary<string, DirectoryInfo>();
 
         Microsoft.Office.Interop.Word.Application WordApplication;
@@ -51,29 +53,29 @@ namespace TutorialBook
         }
 
         private void initLecuresTreeView()
-        {   
+        {
             //переход в папку Lectures
             LecturesDirectory = new DirectoryInfo(AppDirectory.FullName + @"\Lectures\");
-            
+
             //добавление папок с содержанием каждой лекции в список лекций
             foreach (DirectoryInfo dir in LecturesDirectory.GetDirectories())
             {
                 //добавление частей лекций
                 FileInfo[] lectureFiles = dir.GetFiles();
-                List<TreeNode> childNodes=new List<TreeNode>();
-                foreach(FileInfo file in lectureFiles)
+                List<TreeNode> childNodes = new List<TreeNode>();
+                foreach (FileInfo file in lectureFiles)
                 {
                     childNodes.Add(new TreeNode(file.Name));
-                    lectureFilesDictionary.Add(file.Name,file);
+                    lectureFilesDictionary.Add(file.Name, file);
                 }
-                LecturesTreeView.Nodes.Add(new TreeNode(dir.Name,childNodes.ToArray()));
+                LecturesTreeView.Nodes.Add(new TreeNode(dir.Name, childNodes.ToArray()));
             }
         }
 
         private void initExercisesListView()
         {
             ExercisesDirectory = new DirectoryInfo(AppDirectory.FullName + @"\Exercises\");
-            foreach(DirectoryInfo dir in ExercisesDirectory.GetDirectories())
+            foreach (DirectoryInfo dir in ExercisesDirectory.GetDirectories())
             {
                 ExercisesTreeView.Nodes.Add(new TreeNode(dir.Name));
                 exerciseDirectoriesDictionary.Add(dir.Name, dir);
@@ -81,12 +83,12 @@ namespace TutorialBook
         }
 
         private void LecturesTreeView_AfterSelect(object sender, TreeViewEventArgs e)
-        {            
+        {
             if (lectureFilesDictionary.ContainsKey(e.Node.Text))
             {
                 FileInfo file = lectureFilesDictionary[e.Node.Text];
                 loadWordFile(file);
-            }          
+            }
         }
 
         private void ExercisesTreeView_AfterSelect(object sender, TreeViewEventArgs e)
@@ -94,19 +96,28 @@ namespace TutorialBook
             if (exerciseDirectoriesDictionary.ContainsKey(e.Node.Text))
             {
                 DirectoryInfo exerciseDirectory = exerciseDirectoriesDictionary[e.Node.Text];
+                currentExerciseDirectory = exerciseDirectory;
 
                 FileInfo[] exerciseFiles = exerciseDirectory.GetFiles();
 
-                FileInfo exerciseTextFile = exerciseFiles.FirstOrDefault(file=> { return file.Name == "text.txt"; });
+                FileInfo exerciseTextFile = exerciseFiles.FirstOrDefault(file => { return file.Name == "text.txt"; });
                 if (exerciseTextFile != null)
                 {
-                    ExerciseTextTextBox.Text = System.IO.File.ReadAllText(exerciseTextFile.FullName,Encoding.Default);
+                    ExerciseTextTextBox.Text = System.IO.File.ReadAllText(exerciseTextFile.FullName, Encoding.Default);
+                }
+                else
+                {
+                    ExerciseTextTextBox.Clear();
                 }
 
                 FileInfo exerciseTemplateFile = exerciseFiles.FirstOrDefault(file => { return file.Name == "template.cpp"; });
-                if (exerciseTextFile != null)
+                if (exerciseTemplateFile != null)
                 {
                     UserCodeTextBox.Text = System.IO.File.ReadAllText(exerciseTemplateFile.FullName, Encoding.Default);
+                }
+                else
+                {
+                    UserCodeTextBox.Clear();
                 }
             }
         }
@@ -133,7 +144,7 @@ namespace TutorialBook
                 WordDocument.Content.Copy();
 
                 LectureRichTextBox.Clear();
-                LectureRichTextBox.Paste();                
+                LectureRichTextBox.Paste();
             }
             catch (Exception ex)
             {
@@ -148,9 +159,46 @@ namespace TutorialBook
 
         private void RunCodeButton_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("RunCodeButton_Click");
-        }
+            if (currentExerciseDirectory != null)
+            {
+                Process process = new Process();
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    WorkingDirectory = currentExerciseDirectory.FullName,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                };
 
-        
+                process.Start();
+
+                using (StreamWriter pWriter = process.StandardInput)
+                {
+                    if (pWriter.BaseStream.CanWrite)
+                    {
+                        pWriter.WriteLine(@"path=%path%;C:\MinGW\bin");
+                        pWriter.WriteLine(@"mingw32-g++.exe -Wall -fexceptions -g -c template.cpp -o template.o");
+                        pWriter.WriteLine(@"mingw32-g++.exe  -o template.exe template.o");
+                        pWriter.WriteLine(@"template.exe");
+                    }
+                }
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    string line = process.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                while (!process.StandardError.EndOfStream)
+                {
+                    string line = process.StandardError.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                process.WaitForExit();
+            }
+        }
     }
 }
